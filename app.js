92fa72627a4b0c0bebba68494314267d6d0a5d03
younes ait m'ha <younesaitmha@gmail.com>
2022-03-07 13:42:20 +0100

    fix cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import bp from 'body-parser'
+import cors from 'cors'
 //routes
 import authRouter from './routes/auth.js'
 
@@ -7,12 +8,17 @@ const { urlencoded, json } = bp
 //Express
 const app = express()
 app.use(urlencoded({ extended: true }))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(json())
 
 app.use('/auth', authRouter)
 
 app.get('/', (req, res) => {
-    res.json({ status: 'OK' })
+    res.status(200).json({ status: 'OK' })
 })
 
 export default app
